628d915641bcd31e055b615bf62303a7710e9ea2
wd_CN <weidong@gwu.edu>
2019-07-14 18:42:17 -0400

    Updates

---
+++ precache-manifest.f237dc3482b79cec6d32bdc34cd518c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd66ef419833b1d4191e",
+    "url": "/CSCI-6562_Rolling-the-Dice/static/js/runtime~main.cd66ef41.js"
+  },
+  {
+    "revision": "a7ed3a618ebfa95219d8",
+    "url": "/CSCI-6562_Rolling-the-Dice/static/js/main.a7ed3a61.chunk.js"
+  },
+  {
+    "revision": "aebd42729842b37bce5d",
+    "url": "/CSCI-6562_Rolling-the-Dice/static/js/1.aebd4272.chunk.js"
+  },
+  {
+    "revision": "a7ed3a618ebfa95219d8",
+    "url": "/CSCI-6562_Rolling-the-Dice/static/css/main.b2723e89.chunk.css"
+  },
+  {
+    "revision": "aebd42729842b37bce5d",
+    "url": "/CSCI-6562_Rolling-the-Dice/static/css/1.e5192c1c.chunk.css"
+  },
+  {
+    "revision": "ca0681779200e0687d26f3f281a81eb0",
+    "url": "/CSCI-6562_Rolling-the-Dice/index.html"
+  }
+];
